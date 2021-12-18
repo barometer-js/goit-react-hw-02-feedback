7866e7afd62f2789e0ca59c0cf805cc2d3f5a178
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-12-19 01:16:30 +0200

    markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import 'normalize.css';
 import './App.css';
+import Section from './components/Section/Section';
 
-function App() {
-  return <Fragment></Fragment>;
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {};
+
+  countPositiveFeedbackPercentage = () => {};
+
+  render() {
+    return (
+      <Fragment>
+        <Section>
+          <h1>Please leave feedback</h1>
+          <button type="button">Good</button>
+          <button type="button">Neutral</button>
+          <button type="button">Bad</button>
+          <h2>Statistics</h2>
+          <ul>
+            <li>{`Good: ${3}`}</li>
+            <li>{`Neutral: ${5}`}</li>
+            <li>{`Bad: ${2}`}</li>
+          </ul>
+        </Section>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
