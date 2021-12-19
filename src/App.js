52e0911b17923ced5d72da063f1c3d2f19bd5721
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-12-20 01:38:16 +0200

    add notingTotal: 14

--- src/App.js
+++ src/App.js
@@ -5,28 +5,60 @@ import Section from './components/Section/Section';
 
 class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: 10,
+    neutral: 2,
+    bad: 2,
   };
 
-  countTotalFeedback = () => {};
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  };
 
-  countPositiveFeedbackPercentage = () => {};
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  addFeedback = () => {
+    console.log(this.button);
+    // switch (this.button.value) {
+    //   case 'Good':
+    //     this.setState(prevState => ({ good: prevState.good + 1 }));
+    //     break;
+
+    //   case 'Neutral':
+    //     this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+    //     break;
+
+    //   case 'Bad':
+    //     this.setState(prevState => ({ bad: prevState.bad + 1 }));
+    //     break;
+
+    //   default:
+    //     console.log('Invalid subscription type');
+    // }
+  };
 
   render() {
     return (
       <Fragment>
         <Section>
           <h1>Please leave feedback</h1>
-          <button type="button">Good</button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button>
+          <button type="button" onClick={this.addFeedback}>
+            Good
+          </button>
+          <button type="button" onClick={this.addFeedback}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.addFeedback}>
+            Bad
+          </button>
           <h2>Statistics</h2>
           <ul>
-            <li>{`Good: ${3}`}</li>
-            <li>{`Neutral: ${5}`}</li>
-            <li>{`Bad: ${2}`}</li>
+            <li>{`Good: ${this.state.good}`}</li>
+            <li>{`Neutral: ${this.state.neutral}`}</li>
+            <li>{`Bad: ${this.state.bad}`}</li>
+            <li>{`Total: ${this.countTotalFeedback()}`}</li>
+            <li>{`Positive feedback: ${this.countPositiveFeedbackPercentage()}%`}</li>
           </ul>
         </Section>
       </Fragment>
